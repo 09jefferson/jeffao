928771d5d4f50fbccc9c3950868664f712a949c6
09jefferson <vicente.jefferson@escola.pr.gov.br>
2024-09-10 00:05:18 +0000

    jeff

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
+function setup() {
+      createCanvas(400, 400);
+        background("#9E9E9E");
+        }
 
+        function draw() {
+          stroke("#03A9F4");
+            fill("#E91E1E");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 20);
+                          }
+}
